bb804798dea8f695d5b7af7379d385458cc0365f
nikolas_tkn <nikolas@tkn.rs>
2017-06-01 11:18:39 +0200

    IReadable interface added

---
+++ Search_Engine_Assignment/Search_Engine_Assignment/IReadableList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search_Engine_Assignment
+{
+    public interface IReadableList<T>
+    {
+		List<T> GetList<T>();
+    }
+}
