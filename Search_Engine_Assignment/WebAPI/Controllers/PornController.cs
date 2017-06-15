bd7afdd048bb142593eda394a83463d37902835e
nikolas_tkn <nikolas@tkn.rs>
2017-06-15 15:27:35 +0200

    PornController added to WebAPI

---
+++ Search_Engine_Assignment/WebAPI/Controllers/PornController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicesClassLibrary.Base;
+using ServicesClassLibrary.Service;
+using Shared.Base;
+
+namespace WebAPI.Controllers {
+	[Produces("application/json")]
+	[Route("api/[controller]")]
+	public class PornController : Controller {
+
+		// GET api/porn
+		[HttpGet]
+		public List<Porn> Get() {
+
+			var items = new PornService().GetList();
+
+			return items;
+		}
+	}
+}
